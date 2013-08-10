0790b8dd0f891f4f41ee37e5e8ed98e1af52922f
missdeer <devnull@localhost>
2013-08-10 15:33:17 +0800

    (*)change the configure files stored path

--- src/ainesmile/config.cpp
+++ src/ainesmile/config.cpp
@@ -32,30 +32,12 @@ void Config::sync()
 
 QString Config::getConfigDirPath()
 {
-#if defined(Q_OS_MAC)
-    // get config path
 #if QT_VERSION < QT_VERSION_CHECK(5, 0, 0)
-    QString configPath = QDesktopServices::storageLocation(QDesktopServices::DataLocation);
+    QString configPath = QDesktopServices::storageLocation(QDesktopServices::ConfigLocation);
 #else
-    QString configPath = QStandardPaths::writableLocation(QStandardPaths::DataLocation);
+    QString configPath = QStandardPaths::writableLocation(QStandardPaths::ConfigLocation);
 #endif
     configPath.append("/ainesmile");
-#elif defined(Q_OS_WIN)
-    // get config path
-#if QT_VERSION < QT_VERSION_CHECK(5, 0, 0)
-    QString configPath = QDesktopServices::storageLocation(QDesktopServices::DataLocation);
-#else
-    QString configPath = QStandardPaths::writableLocation(QStandardPaths::DataLocation);
-#endif
-#else
-    // get config path
-#if QT_VERSION < QT_VERSION_CHECK(5, 0, 0)
-    QString configPath = QDesktopServices::storageLocation(QDesktopServices::HomeLocation);
-#else
-    QString configPath = QStandardPaths::writableLocation(QStandardPaths::HomeLocation);
-#endif
-    configPath.append("/.config/ainesmile");
-#endif
     QDir dir(configPath);
     if (!dir.exists())
     {
