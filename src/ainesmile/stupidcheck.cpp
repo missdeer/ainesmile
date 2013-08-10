59bc27055be69b6e4b2374ef7985e074a56ed9df
missdeer <devnull@localhost>
2013-08-10 15:14:38 +0800

    (*)finished keygen and register check algorithm

--- src/ainesmile/stupidcheck.cpp
+++ src/ainesmile/stupidcheck.cpp
@@ -9,6 +9,10 @@
 #include <iomanip>
 #include <sstream>
 #include <string>
+#include <cassert>
+#include <limits>
+#include <stdexcept>
+#include <cctype>
 #include <openssl/crypto.h>
 #include <openssl/err.h>
 #include <openssl/rand.h>
@@ -19,6 +23,20 @@
 #include "util.hpp"
 #include "config.h"
 #include "stupidcheck.h"
+
+static const char b64_table[65] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+static const char reverse_table[128] =
+{
+   64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+   64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+   64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 62, 64, 64, 64, 63,
+   52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 64, 64, 64, 64, 64, 64,
+   64,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14,
+   15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 64, 64, 64, 64, 64,
+   64, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+   41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 64, 64, 64, 64, 64
+};
 
 StupidCheck::StupidCheck()
 {
@@ -65,7 +83,7 @@ bool StupidCheck::isValid(const QString& publicPEM) const
     RSA* rsa = NULL;
     BIO *bufio = BIO_new_mem_buf((void*)pubKey.data(), pubKey.size());
     PEM_read_bio_RSA_PUBKEY(bufio, &rsa, 0, NULL);
-    BIO_free(bufio);
+    BIO_free_all(bufio);
 
     if (rsa)
     {
@@ -76,7 +94,8 @@ bool StupidCheck::isValid(const QString& publicPEM) const
         QString username;
         QString licenseCode;
         Config::instance()->loadLicenseInfo(username, licenseCode);
-        RSA_public_decrypt(licenseCode.size(), (const unsigned char *)licenseCode.toStdString().c_str(), sz, rsa, RSA_PKCS1_PADDING);
+        std::string realLicenseCode = base64_decode(licenseCode.toStdString());
+        RSA_public_decrypt(realLicenseCode.size(), (const unsigned char *)realLicenseCode.c_str(), sz, rsa, RSA_PKCS1_PADDING);
         QString original;
         for (int i = 0; i < (username.size() > pinCode.size() ? username.size() : pinCode.size()); i++)
         {
@@ -94,4 +113,66 @@ bool StupidCheck::isValid(const QString& publicPEM) const
 
     return false;
 
+}
+
+std::string StupidCheck::base64_encode(const std::string &bindata) const
+{
+    using std::string;
+    using std::numeric_limits;
+
+    if (bindata.size() > (numeric_limits<string::size_type>::max() / 4u) * 3u) {
+        throw std::length_error("Converting too large a string to base64.");
+    }
+
+    const std::size_t binlen = bindata.size();
+    // Use = signs so the end is properly padded.
+    string retval((((binlen + 2) / 3) * 4), '=');
+    std::size_t outpos = 0;
+    int bits_collected = 0;
+    unsigned int accumulator = 0;
+    const string::const_iterator binend = bindata.end();
+
+    for (string::const_iterator i = bindata.begin(); i != binend; ++i) {
+        accumulator = (accumulator << 8) | (*i & 0xffu);
+        bits_collected += 8;
+        while (bits_collected >= 6) {
+            bits_collected -= 6;
+            retval[outpos++] = b64_table[(accumulator >> bits_collected) & 0x3fu];
+        }
+    }
+    if (bits_collected > 0) { // Any trailing bits that are missing.
+        assert(bits_collected < 6);
+        accumulator <<= 6 - bits_collected;
+        retval[outpos++] = b64_table[accumulator & 0x3fu];
+    }
+    assert(outpos >= (retval.size() - 2));
+    assert(outpos <= retval.size());
+    return retval;
+}
+
+std::string StupidCheck::base64_decode(const std::string &ascdata) const
+{
+    using std::string;
+    string retval;
+    const string::const_iterator last = ascdata.end();
+    int bits_collected = 0;
+    unsigned int accumulator = 0;
+
+    for (string::const_iterator i = ascdata.begin(); i != last; ++i) {
+        const int c = *i;
+        if (std::isspace(c) || c == '=') {
+            // Skip whitespace and padding. Be liberal in what you accept.
+            continue;
+        }
+        if ((c > 127) || (c < 0) || (reverse_table[c] > 63)) {
+            throw std::invalid_argument("This contains characters not legal in a base64 encoded string.");
+        }
+        accumulator = (accumulator << 6) | reverse_table[c];
+        bits_collected += 6;
+        if (bits_collected >= 8) {
+            bits_collected -= 8;
+            retval += (char)((accumulator >> bits_collected) & 0xffu);
+        }
+    }
+    return retval;
 }
--- src/ainesmile/stupidcheck.h
+++ src/ainesmile/stupidcheck.h
@@ -1,11 +1,17 @@
 #ifndef STUPIDCHECK_H
 #define STUPIDCHECK_H
 
+#include <string>
 #include <QString>
+
 class StupidCheck
 {
 private:
     bool isValid(const QString& publicPEM) const;
+
+    std::string base64_encode(const std::string &bindata) const;
+
+    std::string base64_decode(const std::string &ascdata) const;
 public:
     StupidCheck();
     void save(const QString& username, const QString& licenseCode);
