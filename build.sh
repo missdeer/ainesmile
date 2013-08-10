260f6e890b94eeeeadae7c90c8e6bc97111316c4
missdeer <devnull@localhost>
2013-08-10 09:29:58 +0800

    (*)fixed build script for Mac

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ python WidgetGen.py
 cd ../../../..
 qmake -r CONFIG+=release
 hg log -l 1 | head -n 1 | awk '{print $2}' > src/ainesmile/REVISION
-date -R > src/ainesmile/DATE
+date  > src/ainesmile/DATE
 make -j 4
