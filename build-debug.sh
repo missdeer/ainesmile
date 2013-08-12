4ad38584d294a9cbc10ef92cee705b136826380e
missdeer <devnull@localhost>
2013-08-12 01:25:45 -0800

    (+)add build debug variant script

---
+++ build-debug.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+cd 3rdparty/scintilla/qt/ScintillaEdit/
+python WidgetGen.py
+cd ../../../..
+qmake -r CONFIG+=debug 
+hg log -l 1 | head -n 1 | awk '{print $2}' > src/ainesmile/REVISION
+date  > src/ainesmile/DATE
+make -j 4
