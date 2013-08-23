b6d1d1a8e5636c8b10bc87c7b9f9400580b160d6
missdeer <devnull@localhost>
2013-08-23 22:15:19 +0800

    (*)exchange the tab under the mouse cursor

--- src/ainesmile/tabwidget.cpp
+++ src/ainesmile/tabwidget.cpp
@@ -1,13 +1,16 @@
 #include <QMenu>
+#include <QTabBar>
 #include "tabwidget.h"
 
 TabWidget::TabWidget(QWidget *parent) :
     QTabWidget(parent),
     contentMenu_(new QMenu(this))
 {
-    QAction* action = new QAction(tr("Exchange"), this);
+    QAction* action = new QAction(tr("Move to the other side"), this);
     connect(action, SIGNAL(triggered()), this, SIGNAL(exchangeTab()));
     contentMenu_->addAction(action);
+
+    tabBar()->setSelectionBehaviorOnRemove(QTabBar::SelectPreviousTab);
 }
 
 void TabWidget::mousePressEvent(QMouseEvent *event)
@@ -15,7 +18,12 @@ void TabWidget::mousePressEvent(QMouseEvent *event)
     if (event->button() == Qt::RightButton)
     {
         event->accept();
-        contentMenu_->popup(mapToGlobal(event->pos()));
+        int index = tabBar()->tabAt(event->pos());
+        if (index != -1)
+        {
+            setCurrentIndex(index);
+            contentMenu_->popup(mapToGlobal(event->pos()));
+        }
     }
     else
     {
