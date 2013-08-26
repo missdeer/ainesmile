878c631db6b7d69441dd26f628f8baf2d3b75174
missdeer <devnull@localhost>
2013-08-26 16:54:34 +0800

    (+)add find result dock window

--- src/ainesmile/mainwindow.cpp
+++ src/ainesmile/mainwindow.cpp
@@ -196,8 +196,7 @@ void MainWindow::setMenuItemChecked()
 
 void MainWindow::initDockPanes()
 {
-    QDockWidget *dock = new QDockWidget(tr("Find & Replace"), this);
-    dock->setAllowedAreas(Qt::BottomDockWidgetArea);
+    QDockWidget *dock = new QDockWidget(tr("Find/Replace"), this);
     QListWidget* findReplaceList = new QListWidget(dock);
     findReplaceList->addItems(QStringList()
             << "Thank you for your payment which we have received today."
@@ -221,8 +220,31 @@ void MainWindow::initDockPanes()
     addDockWidget(Qt::BottomDockWidgetArea, dock);
     ui->menuView->addAction(dock->toggleViewAction());
 
+    dock = new QDockWidget(tr("Find Result"), this);
+    QListWidget* findResultList = new QListWidget(dock);
+    findResultList->addItems(QStringList()
+                              << "Thank you for your payment which we have received today."
+                              << "Your order has been dispatched and should be with you "
+                              "within 28 days."
+                              << "We have dispatched those items that were in stock. The "
+                              "rest of your order will be dispatched once all the "
+                              "remaining items have arrived at our warehouse. No "
+                              "additional shipping charges will be made."
+                              << "You made a small overpayment (less than $5) which we "
+                              "will keep on account for you, or return at your request."
+                              << "You made a small underpayment (less than $1), but we have "
+                              "sent your order anyway. We'll add this underpayment to "
+                              "your next bill."
+                              << "Unfortunately you did not send enough money. Please remit "
+                              "an additional $. Your order will be dispatched as soon as "
+                              "the complete amount has been received."
+                              << "You made an overpayment (more than $5). Do you wish to "
+                              "buy more items, or should we return the excess to you?");
+    dock->setWidget(findResultList);
+    addDockWidget(Qt::BottomDockWidgetArea, dock);
+    ui->menuView->addAction(dock->toggleViewAction());
+
     dock = new QDockWidget(tr("Project"), this);
-    dock->setAllowedAreas(Qt::LeftDockWidgetArea | Qt::RightDockWidgetArea);
     QListWidget* projectList = new QListWidget(dock);
     projectList->addItems(QStringList()
             << "John Doe, Harmony Enterprises, 12 Lakeside, Ambleton"
