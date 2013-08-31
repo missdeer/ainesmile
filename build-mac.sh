9a4e5cce6c45d94bd880c61986f04b6bc8029588
missdeer <devnull@localhost>
2013-08-31 08:39:40 +0800

    (*)update script for 5.1.1

--- build-mac.sh
+++ build-mac.sh
@@ -3,6 +3,6 @@ BOOST_ROOT=/opt/local/include
 export BOOST_ROOT
 BOOST_LIBS=/opt/local/lib
 export BOOST_LIBS
-PATH=/Users/yangfan/Qt5.1.0/5.1.0/clang_64/bin:$PATH
+PATH=/Users/yangfan/Qt5.1.1/5.1.1/clang_64/bin:$PATH
 export PATH
 ./build.sh mac
