00c97a9aa28573a5087bb7db7b9d2595d9b4d94f
missdeer <devnull@localhost>
2013-10-07 11:32:05 +0800

    (*)sync scintilla

--- build-mac.sh
+++ build-mac.sh
@@ -3,6 +3,6 @@ BOOST_ROOT=/opt/local/include
 export BOOST_ROOT
 BOOST_LIBS=/opt/local/lib
 export BOOST_LIBS
-PATH=/usr/local/Trolltech/Qt-4.8.5/bin:$PATH
+PATH=/usr/local/Qt-5.2.0/bin:$PATH
 export PATH
 ./build.sh mac
