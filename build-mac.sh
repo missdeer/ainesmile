256b7bce2051edebb263efc6ed529145b65a085c
Fan Yang <missdeer@dfordsoft.com>
2015-07-16 10:23:09 +0800

    (-)remove unused menu items

--- build-mac.sh
+++ build-mac.sh
@@ -3,6 +3,6 @@ BOOST_ROOT=/opt/local/include
 export BOOST_ROOT
 BOOST_LIBS=/opt/local/lib
 export BOOST_LIBS
-PATH=/usr/local/Qt-5.2.0/bin:$PATH
+PATH=$HOME/Qt5.5/5.5.0/clang_64/bin:$PATH
 export PATH
 ./build.sh mac
