73bfeaafe0ace2c0a18c709ba8cc1551e0a8a396
fyang3 <fyang3@FYANG3-WIN7HOME>
2016-08-17 14:53:31 +0800

    (*)find replace WIP

--- src/ainesmile/CodeEdit/findreplace.cpp
+++ src/ainesmile/CodeEdit/findreplace.cpp
@@ -39,8 +39,10 @@ bool findInDocument(ScintillaEdit *sci, FindReplaceOption &fro)
         sptr_t lineEnd = sci->lineFromPosition(p.second);
         for (sptr_t line = lineStart; line <=lineEnd; line++)
             sci->ensureVisibleEnforcePolicy(line);
+        return true;
     }
-    return true;
+
+    return false;
 }
 
 bool findAllInDocuments(QList<ScintillaEdit *> &scis, FindReplaceOption &fro)
@@ -75,36 +77,123 @@ bool findAllInDocuments(QList<ScintillaEdit *> &scis, FindReplaceOption &fro)
 
 bool findAllInDirectory(FindReplaceOption &fro)
 {
+    QDir dir(fro.directory);
+    dir.setFilter(QDir::Files | QDir::NoSymLinks);
+    dir.setNameFilters(fro.filters.split(QChar(';')));
+
+    QFileInfoList&& files = dir.entryInfoList();
+    for (auto fi : files)
+    {
+        fi.absoluteFilePath();
+    }
     return true;
 }
 
 bool findAllInDirectories(FindReplaceOption &fro)
 {
+    QDir dir(fro.directory);
+    dir.setFilter(QDir::Files | QDir::NoSymLinks | QDir::NoDotAndDotDot | QDir::AllDirs);
+    dir.setNameFilters(fro.filters.split(QChar(';')));
+    QDirIterator it(dir, QDirIterator::Subdirectories);
+    while (it.hasNext())
+    {
+    }
     return true;
 }
 
 bool replaceInDocument(ScintillaEdit *sci, FindReplaceOption &fro)
 {
-    return true;
+    int flags = 0;
+    if (fro.matchCase)
+        flags |= SCFIND_MATCHCASE;
+    if (fro.matchWholeWord)
+        flags |= SCFIND_WHOLEWORD;
+    if (fro.regexp)
+        flags |= SCFIND_CXX11REGEX;
+    sptr_t start = sci->currentPos();
+    sptr_t end = sci->textLength();
+    if (fro.searchUp)
+        std::swap(start, end);
+    QPair<int, int> p = sci->findText(flags, fro.strToFind.toStdString().c_str(), start, end);
+    if (p.first >= 0)
+    {
+        sci->grabFocus();
+        sci->setSel(p.first, p.second);
+        sci->replaceSel(fro.strReplaceWith.toStdString().c_str());
+        sptr_t newEnd = p.first + fro.strReplaceWith.length();
+        sci->gotoPos(newEnd);
+        sci->setCurrentPos(newEnd);
+
+        sptr_t endStyled = sci->endStyled();
+        if (endStyled < newEnd)
+            sci->colourise(endStyled, newEnd);
+        sptr_t lineStart = sci->lineFromPosition(p.first);
+        sptr_t lineEnd = sci->lineFromPosition(newEnd);
+        for (sptr_t line = lineStart; line <= lineEnd; line++)
+            sci->ensureVisibleEnforcePolicy(line);
+        return true;
+    }
+
+    return false;
 }
 
 bool replaceAllInDocument(ScintillaEdit *sci, FindReplaceOption &fro)
 {
+    int flags = 0;
+    if (fro.matchCase)
+        flags |= SCFIND_MATCHCASE;
+    if (fro.matchWholeWord)
+        flags |= SCFIND_WHOLEWORD;
+    if (fro.regexp)
+        flags |= SCFIND_CXX11REGEX;
+    sptr_t start = 0;
+    sptr_t end = sci->textLength();
+    if (fro.searchUp)
+        std::swap(start, end);
+    QPair<int, int> p = sci->findText(flags, fro.strToFind.toStdString().c_str(), start, end);
+    while (p.first >= 0)
+    {
+        sci->setSel(p.first, p.second);
+        sci->replaceSel(fro.strReplaceWith.toStdString().c_str());
+        sptr_t newEnd = p.first + fro.strReplaceWith.length();
+        sci->setCurrentPos(newEnd);
+
+        sptr_t start = newEnd;
+        sptr_t end = sci->textLength();
+        p = sci->findText(flags, fro.strToFind.toStdString().c_str(), start, end);
+    }
     return true;
 }
 
 bool replaceAllInDocuments(QList<ScintillaEdit *> &scis, FindReplaceOption &fro)
 {
+    std::for_each(scis.begin(), scis.end(), std::bind(&replaceAllInDocument, std::placeholders::_1, std::ref(fro)));
     return true;
 }
 
 bool replaceAllInDirectory(FindReplaceOption &fro)
 {
+    QDir dir(fro.directory);
+    dir.setFilter(QDir::Files | QDir::NoSymLinks);
+    dir.setNameFilters(fro.filters.split(QChar(';')));
+
+    QFileInfoList&& files = dir.entryInfoList();
+    for (auto fi : files)
+    {
+        fi.absoluteFilePath();
+    }
     return true;
 }
 
 bool replaceAllInDirectories(FindReplaceOption &fro)
 {
+    QDir dir(fro.directory);
+    dir.setFilter(QDir::Files | QDir::NoSymLinks | QDir::NoDotAndDotDot | QDir::AllDirs);
+    dir.setNameFilters(fro.filters.split(QChar(';')));
+    QDirIterator it(dir, QDirIterator::Subdirectories);
+    while (it.hasNext()) 
+    {
+    }
     return true;
 }
 
