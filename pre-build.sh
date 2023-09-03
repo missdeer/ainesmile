518f98f76cd45553416c5b28a0a69879b4b3951b
Fan Yang <missdeer@gmail.com>
2023-09-03 20:50:22 +0800

    check python command name

--- pre-build.sh
+++ pre-build.sh
@@ -1,10 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 cd 3rdparty/scintilla/qt/ScintillaEdit/
-python WidgetGen.py
-cd ../../../..
-git rev-parse --short HEAD > src/ainesmile/REVISION
-if [ "$1" = "mac" ]; then
-    date > src/ainesmile/DATE
+python_path=$(which python)
+echo $python_path
+if [ "$python_path"x = ""x ]; then
+	python3 WidgetGen.py
 else
-    date -R  > src/ainesmile/DATE
+	python WidgetGen.py
+fi
+cd ../../../..
+git rev-parse --short HEAD >src/ainesmile/REVISION
+if [ "$1" = "mac" ]; then
+	date >src/ainesmile/DATE
+else
+	date -R >src/ainesmile/DATE
 fi
